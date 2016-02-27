1b06237952faf2e72c983799ee0da5ae0297b6ed
Naithar <devias.naith@gmail.com>
2016-02-27 20:00:01 +0300

    Initial Commit

---
+++ NHSegmentViewTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NHSegmentViewTest
+//
+//  Created by Sergey Minakov on 27.02.16.
+//  Copyright © 2016 Naithar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NHSegmentViewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NHSegmentViewTest
+//
+//  Created by Sergey Minakov on 27.02.16.
+//  Copyright © 2016 Naithar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
